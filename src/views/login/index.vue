fee84421adf56a96a013ffcf07d00f651a1b4abf
khatung <2976839921@qq.com>
2024-08-23 16:40:41 +0800

    test

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -60,7 +60,7 @@
 </template>
 
 <script>
-// import { isvalidUsername } from "@/utils/validate";
+// import { isvalidUsername } from "@/utils/validate";11
 import { getPicCode } from "../../api/login";
 import { login } from "../../api/login";
 import { Message } from "element-ui";
