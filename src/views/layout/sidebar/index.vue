ea004e5c4dcfc2d2eaeae1f859ec579bf0978ca6
khatung <2976839921@qq.com>
2024-08-24 16:59:31 +0800

    11th modification: Routing path change, student management full checkbox, add name

--- src/views/layout/sidebar/index.vue
+++ src/views/layout/sidebar/index.vue
@@ -1,10 +1,17 @@
 <template>
-    <el-menu :default-active="activeMenu" :unique-opened="true" :collapse="collapse" style="position: absolute;">
+  <el-menu
+    :default-active="activeMenu"
+    router
+    :unique-opened="true"
+    :collapse="collapse"
+    style="position: absolute"
+    active-text-color="#6da2bf"   
+  >
     <!-- 对items进行遍历 -->
     <template v-for="item in sidebarData">
       <!-- 使用v-if判定是否具有第二级菜单 -->
       <template v-if="item.children && item.children.length > 0">
-        <el-submenu :key="item.id" :index="item.id.toString()">
+        <el-submenu :key="item.id" :index="item.path">
           <template slot="title">
             <i :class="item.meta.icon"></i>
             <!-- 如果存在第二级菜单，渲染第一级菜单标题 -->
@@ -16,7 +23,7 @@
             <el-submenu
               v-if="subItem.children && subItem.children.length > 0"
               :key="subItem.id"
-              :index="subItem.id.toString()"
+              :index="subItem.path"
             >
               <i :class="subItem.meta.icon"></i>
               <!-- 如果存在第三级菜单，渲染第二级菜单标题 -->
@@ -25,19 +32,13 @@
               <el-menu-item
                 v-for="(threeItem, i) in subItem.children"
                 :key="i"
-                :index="threeItem.id.toString()"
-                @click="handleMenuSelect(threeItem.component)"
+                :index="threeItem.path"
                 >{{ threeItem.meta.title }}</el-menu-item
               >
             </el-submenu>
 
             <!-- 如果不存在第三级菜单，渲染第二级菜单标题 -->
-            <el-menu-item
-              :key="subItem.id"
-              :index="subItem.id.toString()"
-              @click="handleMenuSelect(subItem.component)"
-              v-else
-            >
+            <el-menu-item :key="subItem.id" :index="subItem.path" v-else>
               <i :class="subItem.meta.icon"></i
               >{{ subItem.meta.title }}</el-menu-item
             >
@@ -46,7 +47,7 @@
       </template>
       <!-- 如果不存在第二级菜单，渲染第一级菜单标题 -->
       <template v-else>
-        <el-menu-item @click="handleMenuSelect(item.component)" :key="item.id" :index="item.id.toString()">
+        <el-menu-item :key="item.id" :index="item.path">
           <i :class="item.meta.icon"></i>
           <span slot="title">{{ item.meta.title }}</span>
         </el-menu-item>
@@ -58,7 +59,7 @@
 <script>
 // import SideMenu from "./sidebarItem.vue";
 import { initSidebar } from "../../../api/sidebar";
-import bus from '@/utils/bus'
+import bus from "@/utils/bus";
 
 export default {
   // components: {
@@ -73,10 +74,10 @@ export default {
   },
   created() {
     this.init();
-    bus.$on('collapse',(msg) => {
+    bus.$on("collapse", (msg) => {
       this.collapse = msg;
       bus.$emit("collapse-content", msg);
-    })
+    });
   },
   methods: {
     async init() {
@@ -99,19 +100,19 @@ export default {
       this.sidebarData = result;
       console.log(this.sidebarData, "sidebarData111");
     },
-    handleMenuSelect(val) {
-      // debugger;
-      this.$router.push(`/layout/${val}`);
-      console.log(val, "val");
-      this.activeMenu = val;
-    },
+    // handleMenuSelect(val) {
+    //   // debugger;
+    //   this.$router.push(`/${val}`);
+    //   console.log(val, "val");
+    //   this.activeMenu = val;
+    // },
   },
 };
 </script>
 <style rel="stylesheet/scss" lang="scss" scoped>
 .nest-menu .el-submenu > .el-submenu__title,
 .el-submenu .el-menu-item {
-  min-width:180px !important;
+  min-width: 180px !important;
 }
 .el-menu--collapse .el-menu .el-submenu {
   min-width: 180px !important;
