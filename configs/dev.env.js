53b3e62681441c1136c63cb5f86f6e1076df5bf7
khatung <2976839921@qq.com>
2024-09-29 18:56:26 +0800

    commit

--- configs/dev.env.js
+++ configs/dev.env.js
@@ -3,5 +3,6 @@ const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"'
+  NODE_ENV: '"development"',
+  BASE_URL: '"//8.154.36.180:8903"'
 })
